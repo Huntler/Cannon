8c25eb440808259b396ffdb2f05a5cc2950579de
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2021-10-21 20:14:17 +0200

    fixed move system

--- stupid_engine/cannon/ai/move_generator.py
+++ stupid_engine/cannon/ai/move_generator.py
@@ -13,7 +13,7 @@ class MoveGenerator:
     def generate_moves(self, player, enemy, soldier=None) -> List[Move]:
         self.refresh()
         if soldier:
-            self._generate_moves(player, enemy, soldier)
+            self._generate_moves(player, enemy, soldier, for_ai=False)
             return self._moves
 
         for soldier in player.get_soldiers().values():
@@ -25,7 +25,7 @@ class MoveGenerator:
         
         return self._moves
        
-    def _generate_moves(self, player, enemy, soldier) -> Move:
+    def _generate_moves(self, player, enemy, soldier, for_ai=True) -> Move:
         # determine the direction in which the current player is playing
         d = -1 if player.get_type() == PlayerType.LIGHT else +1
         x, y = soldier.get_pos()
@@ -44,9 +44,13 @@ class MoveGenerator:
             # other moves are not interesting if the player can finish the game
             town = enemy.get_town().get_pos() == move
             if town:
-                # moves.insert(0, Move(pos=move, soldier=soldier.get_pos(), finish=town))
-                self._moves = [Move(pos=move, soldier=soldier.get_pos(), finish=town)]
-                return
+                town_move = Move(pos=move, soldier=soldier.get_pos(), finish=town)
+                if for_ai:
+                    self._moves = [town_move]
+                    return
+                else:
+                    self._moves.append[town_move]
+                    continue
 
 
             kill = enemy.soldier_at(move)
@@ -62,9 +66,14 @@ class MoveGenerator:
             # other moves are not interesting if the player can finish the game
             town = enemy.get_town().get_pos() == move
             if town:
-                # moves.insert(0, Move(pos=move, soldier=soldier.get_pos(), finish=town))
-                self._moves = [Move(pos=move, soldier=soldier.get_pos(), finish=town)]
-                return
+                town_move = Move(pos=move, soldier=soldier.get_pos(), finish=town)
+                if for_ai:
+                    # moves.insert(0, Move(pos=move, soldier=soldier.get_pos(), finish=town))
+                    self._moves = [town_move]
+                    return
+                else:
+                    self._moves.append[town_move]
+                    continue
 
             kill = enemy.soldier_at(move)
             if kill:
@@ -81,18 +90,20 @@ class MoveGenerator:
         # [soldiers needed for cannon; possible positions for shoots; 
         #   free position for shoot; slide position]
         cannon_cases = [
+
             # orthogonal row, given soldier front
-            [[(x, y - d), (x, y - 2 * d)], [(x, y + 2 * d), (x, y + 3 * d)], (x, y + d), (x, y - 3 * d)],
+            [[(x, y), (x, y - 2)], [(x, y + 2), (x, y + 3)], (x, y + 1), (x, y - 3)],
             # 3 in a orthogonal row, given soldier back
-            [[(x, y + d), (x, y + 2 * d)], [(x, y - 2 * d), (x, y - 3 * d)], (x, y - d), (x, y + 3 * d)],
+            [[(x, y), (x, y + 2)], [(x, y - 2), (x, y - 3)], (x, y - 1), (x, y + 3)],
+
             # 3 in a diagonal row, given soldier front (right)
-            [[(x - 1, y - d), (x - 2, y - 2 * d)], [(x + 2, y + 2 * d), (x + 3, y + 3 * d)], (x + d, y + d), (x - 3, y - 3 * d)],
-            # 3 in a diagonal row, given soldier back (right)
-            [[(x + 1, y + d), (x + 2, y + 2 * d)], [(x - 2, y - 2 * d), (x - 3, y - 3 * d)], (x - d, y - d), (x + 3, y + 3 * d)],
+            [[(x - 1, y - 1), (x - 2, y - 2)], [(x + 2, y + 2), (x + 3, y + 3)], (x + 1, y + 1), (x - 3, y - 3)],
+            # 3 in a diagonal row bottom-left to top-right
+            [[(x + 1, y + 1), (x + 2, y + 2)], [(x - 2, y - 2), (x - 3, y - 3)], (x - 1, y - 1), (x + 3, y + 3)],
             # 3 in a diagonal row, given soldier back (left)
-            [[(x - 1, y + d), (x - 2, y + 2 * d)], [(x + 2, y - 2 * d), (x + 3, y - 3 * d)], (x + d, y - d), (x - 3, y + 3 * d)],
+            [[(x - 1, y + 1), (x - 2, y + 2)], [(x + 2, y - 2), (x + 3, y - 3)], (x + 1, y - 1), (x - 3, y + 3 )],
             # 3 in a diagonal row, given soldier front (left)
-            [[(x + 1, y - d), (x + 2, y - 2 * d)], [(x - 2, y + 2 * d), (x - 3, y + 3 * d)], (x - d, y + d), (x + 3, y - 3 * d)]
+            [[(x + 1, y - 1), (x + 2, y - 2)], [(x - 2, y + 2), (x - 3, y + 3)], (x - 1, y + 1), (x + 3, y - 3)]
         ]
 
         for structure, shots, free, slide in cannon_cases:
@@ -126,9 +137,14 @@ class MoveGenerator:
                 # other moves are not interesting if the player is able to end the game
                 town = enemy.get_town().get_pos()
                 if town == shot:
-                    # moves.insert(0, Move(pos=shot, soldier=soldier.get_pos(), finish=True, shoot=True))
-                    self._moves = [Move(pos=shot, soldier=soldier.get_pos(), finish=True, shoot=True)]
-                    return
+                    town_move = Move(pos=move, soldier=soldier.get_pos(), finish=town, shoot=True)
+                    if for_ai:
+                        # moves.insert(0, Move(pos=move, soldier=soldier.get_pos(), finish=town))
+                        self._moves = [town_move]
+                        return
+                    else:
+                        self._moves.append[town_move]
+                        continue
                 
                 # if the shoot wil hit an enemy soldier, then mark this move as 
                 # shoot/kill and break
@@ -136,7 +152,6 @@ class MoveGenerator:
                 if kill:
                     self._moves.append(Move(pos=shot, soldier=soldier.get_pos(), kill=kill.get_pos(), shoot=True))
                     break
-            
 
         #
         #   RETREAT MOVES
@@ -151,7 +166,6 @@ class MoveGenerator:
         ]
         for threat in [(x - 1, y + d), (x, y + d), (x + 1, y + d), (x - 1, y), (x + 1, y)]:
             if enemy.soldier_at(threat):
-                # TODO: implement light of sight
                 for move, free in retreat_pos:
                     if Move.out_of_bounds(move) or player.get_town().get_pos() == move or player.soldier_at(free) or enemy.soldier_at(free):
                         continue
